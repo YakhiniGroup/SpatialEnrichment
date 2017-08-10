27b8d3291b9b986a7f7525f99b7756d2221fcb32
asaf <asafried@hotmail.com>
2017-08-10 12:03:13 +0300

    asaf commit

--- AzureFuncDatabaseQuery/AzureDatabaseQueryFunc/AzureDatabaseQueryFunc/Function.cs
+++ AzureFuncDatabaseQuery/AzureDatabaseQueryFunc/AzureDatabaseQueryFunc/Function.cs
@@ -29,22 +29,34 @@ namespace DatabaseProgressQuery
             try
             {
                 DatabaseHandler handler = new DatabaseHandler();
+                //Query testQuery1 = new Query { Id = "1", Value = 0, Message = "Proccess Started" };
+                //Query testQuery2 = new Query { Id = "2", Value = 50, Message = "Proccessed data" };
+                //Query testQuery3 = new Query { Id = "3", Value = 100, Message = "Proccess Complete" };
+                //await handler.CreateQueryDocumentIfNotExistsAsync(testQuery1);
+                //await handler.CreateQueryDocumentIfNotExistsAsync(testQuery2);
+                //await handler.CreateQueryDocumentIfNotExistsAsync(testQuery3);
                 query = handler.SearchForQuery(id);
+                //await handler.DeleteQueryDocumentAsync("1");
+                //await handler.DeleteQueryDocumentAsync("2");
+                //await handler.DeleteQueryDocumentAsync("3");
             }
             catch (DocumentClientException de)
             {
                 Exception baseException = de.GetBaseException();
+                log.Info("---ERROR DocumentClientException---");
                 log.Info(String.Format("{0} error occurred: {1}, Message: {2}", de.StatusCode, de.Message, baseException.Message));
             }
             catch (Exception e)
             {
+                log.Info("---ERROR Exception---");
                 Exception baseException = e.GetBaseException();
                 log.Info(String.Format("Error: {0}, Message: {1}", e.Message, baseException.Message));
             }
             finally
             {
+                Query notFoundQuery = new Query { Id = "-1", Value = 100, Message = "" };
                 responseMesssage = query == null
-                    ? req.CreateResponse(HttpStatusCode.BadRequest, "Error while querying data base")
+                    ? req.CreateResponse(HttpStatusCode.OK, notFoundQuery)
                     : req.CreateResponse(HttpStatusCode.OK, query);
             }
 
